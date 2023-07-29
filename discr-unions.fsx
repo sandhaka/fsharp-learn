efd55f291ead949c2f8556d92f549f0ae94a8dc7
Matteo Tosato <matteo.tosato7@gmail.com>
2023-07-29 16:14:21 +0200

    discr union

--- discr-unions.fsx
+++ discr-unions.fsx
@@ -10,10 +10,42 @@ let i  = I 99    // use the "I" constructor
 let b  = B true  // use the "B" constructor
 // val b : IntOrBool = B true
 
-// The component types can be any other type you like, including tuples, records, other union types, and so on.
+// The component types can be any other type you like, including tuples,
+// records, other union types, and so on.
 type Person = { first : string; last : string }
 type MixedType =
     | Tup of int * int
     | P of Person
     | L of int list
     | U of IntOrBool
+let personValue = P { first = "John"; last = "Smith" }
+let tupleValue = Tup (1, 2)
+let listValue = L [1; 2; 3]
+let unionValue = U (I 99)
+let unionValue2 = U (B true)
+// Is much better to explicitly qualify the type:
+personValue = MixedType.P { first = "John"; last = "Smith" }
+
+
+// Matching and deconstructing
+let matcher x =
+    match x with
+    | Tup (a, b) ->
+        printfn $"Tuple: (%d{a}, %d{b})"
+    | P { first = f; last = l } ->
+        printfn $"Person: %s{f} %s{l}"
+    | L list ->
+        printfn $"List of int: %A{list}"
+    | U intOrBool ->
+        printfn "IntOrBool: "
+        match intOrBool with
+        | I i ->
+            printfn $"  Integer: %d{i}"
+        | B b ->
+            printfn $"  Boolean: %b{b}"
+// Use the matcher function
+matcher personValue
+matcher tupleValue
+matcher listValue
+matcher unionValue
+matcher unionValue2
